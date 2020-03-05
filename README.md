b65904905dccc49babbad505d187fc3293c38142
beelzebule <beelzebule@naver.com>
2020-03-05 21:11:04 +0900

    #3.11 Install ESLint

--- README.md
+++ README.md
@@ -235,3 +235,5 @@ Why we need dotenv?
 ### Model
 
 ### multer
+
+### ESLint
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,10 +1,9 @@
 import routes from "../routes";
 import Video from "../models/Video";
-import { Mongoose } from "mongoose";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ _id: -1 });
     res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     console.log(error);
